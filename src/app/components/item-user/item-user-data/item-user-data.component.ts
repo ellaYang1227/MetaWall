6ba91d8bd028e8e12815ff78852a528a2055d2f5
ella <yanzi19841227@gmail.com>
2022-06-19 20:17:33 +0800

    修改個人頁面點擊留言使用者 無法關閉 spinner

--- src/app/components/item-user/item-user-data/item-user-data.component.ts
+++ src/app/components/item-user/item-user-data/item-user-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '@services/auth.service';
 
 @Component({
@@ -6,8 +7,9 @@ import { AuthService } from '@services/auth.service';
   templateUrl: './item-user-data.component.html',
   styleUrls: ['./item-user-data.component.scss']
 })
-export class ItemUserDataComponent implements OnInit {
+export class ItemUserDataComponent implements OnInit, OnChanges {
   userDefaultImg = this.authService.userDefaultImg;
+  isActiveLink = false;
 
   @Input() user!: any;
   @Input() createdAt!: Date;
@@ -17,10 +19,15 @@ export class ItemUserDataComponent implements OnInit {
 
 
   constructor(
+    private route: ActivatedRoute,
     private authService: AuthService
   ) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.isActiveLink = this.user._id === this.route.snapshot.paramMap.get('id');
   }
 
 }
